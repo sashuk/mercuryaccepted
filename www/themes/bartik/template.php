524d6d69a730c9a2e2dd098b54ccf483eafb7ab8
Alex Shumilov <shumsan1011@gmail.com>
2013-10-15 23:00:53 +0900

    Friendly user login

--- www/themes/bartik/template.php
+++ www/themes/bartik/template.php
@@ -344,3 +344,17 @@ function bartik_field__field_projects($variables) {
 	}
 	return $output;
 }
+
+function bartik_theme(&$existing, $type, $theme, $path) {
+  $hooks['user_login'] = array(
+    'template' => 'user_login',
+    'render element' => 'form',
+    // other theme registration code...
+  );
+  return $hooks;
+}
+
+function bartik_preprocess_user_login(&$variables) {
+  $variables['intro_text'] = t('This is my awesome login form');
+  $variables['rendered'] = drupal_render_children($variables['form']);
+}
---
+++ www/themes/bartik/user_login.tpl.php
@@ -0,0 +1,10 @@
+<div class="tmpl_sector_header_bottom c_padbot100">
+    <div class="sector_bottom_glare"></div>
+    <div class="centered_wrap">
+    <div class="middle_wrap">
+		<div class="login-form">
+		  <?php print $rendered; ?>
+		</div>
+    </div>
+    </div>
+</div>
